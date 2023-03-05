60dde9c91aee10ec561f9a4c46f286be2a86bff0
zana-abdollahpour <zana.ap4@gmail.com>
2023-03-05 18:54:00 +0330

    add basic layout and cleanup

---
+++ components/Layout.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + " - Amazona" : "Amazona"}</title>
+        <meta name="description" content="ECommerce website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between shadow-md items-center px-4">
+            <Link className="text-lg font-bold" href="/">
+              Amazona
+            </Link>
+            <div>
+              <Link className="p-2" href="/cart">
+                Cart
+              </Link>
+              <Link className="p-2" href="/login">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex justify-center items-center h-10 shadow-inner">
+          Copyright &#169; 2023 Amazona
+        </footer>
+      </div>
+    </>
+  );
+}
