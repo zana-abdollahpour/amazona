6db05ff4d34e0cd63d707cbc80b208ed7ea6ccb6
zana-abdollahpour <zana.ap4@gmail.com>
2023-03-11 13:53:35 +0330

    add react-chartjs-2, chart.js and admin dashbord page

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function App({ Component, pageProps: { session, ...pageProps } }) {
       <StoreProvider>
         <PayPalScriptProvider deferLoading={true}>
           {Component.auth ? (
-            <Auth>
+            <Auth adminOnly={Component.auth.adminOnly}>
               <Component {...pageProps} />
             </Auth>
           ) : (
@@ -22,15 +22,18 @@ function App({ Component, pageProps: { session, ...pageProps } }) {
   );
 }
 
-function Auth({ children }) {
+function Auth({ children, adminOnly }) {
   const router = useRouter();
-  const { status } = useSession({
+  const { status, data: session } = useSession({
     required: true,
     onUnauthenticated() {
       router.push("/unauthorized?message=login required");
     },
   });
   if (status === "loading") return <div>Loading...</div>;
+  if (adminOnly && !session.user.isAdmin)
+    router.push("/unauthorized?message=admin login required");
+
   return children;
 }
 
---
+++ pages/admin/dashboard.js
@@ -0,0 +1,158 @@
+import React, { useEffect, useReducer } from "react";
+import Link from "next/link";
+import axios from "axios";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import Layout from "@/components/Layout";
+import { getError } from "@/utils/error";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, summary: action.payload, error: "" };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
+
+function AdminDashboardScreen() {
+  const [{ loading, error, summary }, dispatch] = useReducer(reducer, {
+    loading: true,
+    summary: { salesData: [] },
+    error: "",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get("/api/admin/summary");
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (error) {
+        dispatch({
+          type: "FETCH_FAIL",
+          payload: getError(error),
+        });
+      }
+    };
+    fetchData();
+  }, []);
+
+  const data = {
+    labels: summary.salesData.map((x) => x._id),
+    datasets: [
+      {
+        label: "Sales",
+        backgroundColor: "rgba(162,222,208,1)",
+        data: summary.salesData.map((x) => x.totalSales),
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Layout title="Admin Dashboard">
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div>
+            <ul>
+              <li>
+                <Link href="/admin/dashboard" className="font-bold">
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link href="/admin/orders" className="font-bold">
+                  Orders
+                </Link>
+              </li>
+              <li>
+                <Link href="/admin/products" className="font-bold">
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link href="/admin/users" className="font-bold">
+                  Users
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="md:col-span-3">
+            <h1 className="mb-4 text-xl">Admin Dashboard</h1>
+            {loading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div className="alert-error">{error}</div>
+            ) : (
+              <div>
+                <div className="grid grid-cols-1 md:grid-cols-4">
+                  <div className="p-5 m-5 card">
+                    <p className="text-3xl">{summary.ordersPrice}</p>
+                    <p>Sales</p>
+                    <Link href="/admin/orders">View Sales</Link>
+                  </div>
+                  <div className="p-5 m-5 card">
+                    <p className="text-3xl">{summary.ordersCount}</p>
+                    <p>Orders</p>
+                    <Link href="/admin/orders">View Orders</Link>
+                  </div>
+                  <div className="p-5 m-5 card">
+                    <p className="text-3xl">{summary.productsCount}</p>
+                    <p>Products</p>
+                    <Link href="/admin/products">View products</Link>
+                  </div>
+                  <div className="p-5 m-5 card">
+                    <p className="text-3xl">{summary.usersCount}</p>
+                    <p>Users</p>
+                    <Link href="/admin/users">View users</Link>
+                  </div>
+                </div>
+                <h2 className="text-xl">Sales Report</h2>
+                <Bar
+                  options={{
+                    legend: { display: true, position: "right" },
+                  }}
+                  data={data}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+}
+
+AdminDashboardScreen.auth = { adminOnly: true };
+export default AdminDashboardScreen;
