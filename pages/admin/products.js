8039bf7ae4726d8b6c49346a920a75baaecaf11e
zana-abdollahpour <zana.ap4@gmail.com>
2023-03-17 19:36:16 +0330

    add products page for admin

---
+++ pages/admin/products.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useReducer } from "react";
+import axios from "axios";
+
+import { getError } from "../../utils/error";
+import Layout from "../../components/Layout";
+import Link from "next/link";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, error: "", orders: action.payload };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
+
+export default function AdminProductsScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    loading: true,
+    products: [],
+    error: "",
+  });
+
+  const deleteHandler = () => {
+    // Implement it later
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get(`/api/admin/products`);
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(error) });
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Layout title="Admin Products">
+      <div className="grid md:grid-cols-4 md:gap-5">
+        <div>
+          <ul>
+            <li>
+              <Link href="/admin/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/admin/orders">Orders</Link>
+            </li>
+            <li>
+              <Link href="/admin/products" className="font-bold">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin/users">Users</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="overflow-x-auto md:colspan3">
+          <h1 className="mb-4 text-xl">Products</h1>
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div className="alert-error">{error}</div>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">ID</th>
+                    <th className="p-5 text-left">NAME</th>
+                    <th className="p-5 text-left">PRICE</th>
+                    <th className="p-5 text-left">CATEGORY</th>
+                    <th className="p-5 text-left">COUNT</th>
+                    <th className="p-5 text-left">RATING</th>
+                    <th className="p-5 text-left">ACTIONS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr key={product._id} className="border-b">
+                      <td className="p-5 ">{product._id.substring(20, 24)}</td>
+                      <td className="p-5 ">{product.name}</td>
+                      <td className="p-5 ">${product.price}</td>
+                      <td className="p-5 ">{product.category}</td>
+                      <td className="p-5 ">{product.countInStock}</td>
+                      <td className="p-5 ">{product.rating}</td>
+                      <td className="p-5 ">
+                        <Link
+                          href={`/admin/product/${product._id}`}
+                          type="button"
+                          className="default-button"
+                        >
+                          Edit
+                        </Link>
+                        &nbsp;
+                        <button
+                          onClick={() => deleteHandler(product._id)}
+                          className="default-button"
+                          type="button"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+AdminProductsScreen.auth = { adminOnly: true };
